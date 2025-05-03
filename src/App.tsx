7ed87c3deafc5dfc4671b244f217c7eafe9b2f56
tkk2606 <kieuth24@gmail.com>
2025-05-03 10:26:45 -0700

    import statement

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
-import { BrowserRouter as Router } from 'react-router-dom';
-import AppRoutes from './routes';
-import Navbar from './components/layout/Navbar';
-import Footer from './components/layout/Footer';
+import { BrowserRouter as Router } from "react-router-dom";
+import AppRoutes from "./routes";
+import Navbar from "./components/layout/Navbar";
+import Footer from "./components/layout/Footer";
 
 function App() {
   return (
