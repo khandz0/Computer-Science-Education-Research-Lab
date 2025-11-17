265b093f8b6427b96aae0fbd827b46ae3f76cf8e
tkk2606 <kieuth24@gmail.com>
2025-11-16 17:32:27 -0800

    Fix file directory issue in BilingualWorkshopPage

--- src/pages/BilingualWorkshopPage.tsx
+++ src/pages/BilingualWorkshopPage.tsx
@@ -3,7 +3,7 @@ import PageHeader from "../components/common/PageHeader";
 import Container from "../components/common/Container";
 import Section from "../components/common/Section";
 import workshop2 from "../assets/workshop/workshop2.jpeg";
-import poster2 from "../assets/SIGCSE Poster.png";
+import poster2 from "../assets/posters/SIGCSE Poster.png";
 import workshop from "../assets/workshop/workshop.jpeg";
 import poster1 from "../assets/researchday/poster.jpg";
 import codeinmandarin from "../assets/workshop/codeinmandarin.png";
